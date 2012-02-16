7cf694eedaa55241a0561ba04a8bd6374b148c81
Melissa Binde <mbinde@gmail.com>
2012-02-16 19:59:16 +0000

    Use a path instead of the server name to work with the test server

--- js/choose-date.js
+++ js/choose-date.js
@@ -4,7 +4,16 @@
    			onSelect: function(dateText, inst) {
    				$("#date-details").html('Loading schedule for <strong>' + dateText + '</strong>...');
    				path = window.location.pathname.split("/");
-   				$("#date-details").load("http://www.elitetutorsonline.com/callback/schedule", {date:dateText,user:path[2]});
+   				$("#date-details").load("/callback/schedule", {date:dateText,user:path[2],path1:path[1]});
+   			}
+		});
+	});
+	$(function() {
+		$("#show-calendar").datepicker({
+   			onSelect: function(dateText, inst) {
+   				$("#date-details").html('Loading schedule for <strong>' + dateText + '</strong>...');
+   				path = window.location.pathname.split("/");
+   				$("#date-details").load("/callback/calendar", {date:dateText,user:path[2],path1:path[1]});
    			}
 		});
 	});
